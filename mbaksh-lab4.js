48187e1c0f94ffec79798ce00fd88992ea14e8c4
Mudassir-13 <mbaksh@hawk.iit.edu>
2026-04-01 20:59:11 -0500

    chaged the text of <p> and made two words <b> and <i>

--- mbaksh-lab4.js
+++ mbaksh-lab4.js
@@ -6,3 +6,8 @@
 (function () { 
     document.querySelector("#hero h1").textContent = "Uplift Your Brand with Stellar Marketing"; 
 })();
+
+//2A
+(function () { 
+    document.querySelector("#hero p").innerHTML = "Utilize cutting-edge strategies from Stellar Marketing to help your business <b><i>thrive</i></b> and <b><i>excel</i></b>.";
+})();
