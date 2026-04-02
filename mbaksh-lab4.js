47ea3573de35fa25e112fa26f69ae8a9c36620fc
Mudassir-13 <mbaksh@hawk.iit.edu>
2026-04-01 22:00:02 -0500

    changed the background image

--- mbaksh-lab4.js
+++ mbaksh-lab4.js
@@ -7,7 +7,16 @@
     document.querySelector("#hero h1").textContent = "Uplift Your Brand with Stellar Marketing"; 
 })();
 
-//2A
+//1B
 (function () { 
     document.querySelector("#hero p").innerHTML = "Utilize cutting-edge strategies from Stellar Marketing to help your business <b><i>thrive</i></b> and <b><i>excel</i></b>.";
 })();
+
+//1C
+(function () { 
+    document.querySelector("#hero").style.backgroundImage = "url('https://fastly.picsum.photos/id/683/1280/720.jpg?hmac=MorP0xeZ8ldA3Xsf3cmIbrsN-jz3Omu7piIVSyv3jMk')";
+})();
+
+//1D
+
+//1E
